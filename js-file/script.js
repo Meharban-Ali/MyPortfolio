d4cdedbf14d786418f8af8a4f4c4960eec521a0d
Meharban-Ali <meharbanali7900@gmail.com>
2023-12-12 18:24:37 +0530

    files move new folder

---
+++ js-file/script.js
@@ -0,0 +1,6 @@
+var typed = new  Typed(".typing",{
+    strings:["Web Developer......","Software Developer.....","Web Developer..... Front-End Developer"],
+    typeSpeed:40,
+    BackSpeed:90,
+    loop:true
+})
